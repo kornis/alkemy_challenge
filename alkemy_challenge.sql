015960320d1b6a7cf9cb224226a57ac85dd10316
Federico Garcia <fedegarcia222@gmail.com>
2021-01-23 02:47:03 -0300

    Se hace merge de la rama pruebas con los cambios realizados

--- alkemy_challenge.sql
+++ alkemy_challenge.sql
@@ -1,5 +1,4 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -15,21 +14,44 @@ CREATE TABLE `budget` (
   `qty` int(11) NOT NULL,
   `type` enum('in','out') COLLATE utf8_spanish_ci NOT NULL,
   `date` date NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
-INSERT INTO `budget` (`id`, `movement_name`, `qty`, `type`, `date`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(45, 'Sueldo', 60000, 'in', '2021-01-16', '2021-01-16 03:24:39', '2021-01-16 03:24:39', NULL);
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `email` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `token` text COLLATE utf8_spanish_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+INSERT INTO `users` (`id`, `email`, `password`, `token`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(24, 'fede@fede.com', '$2b$12$tmz4cAlpcylCrzfG6QEfiuK5uk77j0/qIqGHXKxvpvd.vemmD1skG', NULL, '2021-01-23 05:02:32', '2021-01-23 05:02:32', NULL);
 
 
 ALTER TABLE `budget`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `movement_user` (`user_id`);
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 
 ALTER TABLE `budget`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+
+ALTER TABLE `budget`
+  ADD CONSTRAINT `movement_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
